5a9f5c23fe9baff18cebcb93c8859f4867f84a0c
Nestor Garcia <es717249@iteso.mx>
2019-04-03 06:38:04 -0600

    modification of SW LW test asm

--- ROM_codes/Test_MIPS_SW_LW.asm
+++ ROM_codes/Test_MIPS_SW_LW.asm
@@ -1,8 +1,8 @@
 .text 
 
 	add $t0,$t0,0x03
-	lui $s0,0x1000	
-	ori $s0,$s0,0x4000
+	lui $s0,0x1001
+	ori $s0,$s0,0x0000
 	
 	sw  $t0,0,($s0)		#store 3 in 0x10010001
 	lw  $s1,0,($s0)		#load $s0 in $s1
@@ -10,8 +10,8 @@
 	addi $s1,$s1,0
 
 #Writing to GPIO	
-	lui $s2,0x1000
-	ori $s2,$s2,0x4024
+	lui $s2,0x1001
+	ori $s2,$s2,0x0024
 	
 	sw $t0,0,($s2)
 	
